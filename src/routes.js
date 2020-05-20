80327d35688e5d5a2474eaf0a17c36746fc95ab8
hiagopsilva <hiagohps360@gmail.com>
2020-05-20 18:47:16 -0300

    add NavigationContainer and import pages

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const AppStack = createStackNavigator();
 
-const Routes = () => (
-  <AppStack.Navigator>
-    <AppStack.Screen  />
-  </AppStack.Navigator>
-)
+import Welcome from './pages/Welcome';
+import Timer from './pages/Timer';
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator>
+        <AppStack.Screen  name="welcome" component={Welcome} />
+        <AppStack.Screen  name="Timer" component={Timer} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Routes;
