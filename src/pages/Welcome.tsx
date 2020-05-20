a7ffd7c56c9ad430044dbdf1f38250136e3812e9
hiagopsilva <hiagohps360@gmail.com>
2020-05-20 19:29:23 -0300

    added components and styles

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-// import { Container } from './styles';
+import bannerImg from '../assets/banner.png';
 
 const Welcome: React.FC = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  function navigateToTimer() {
+    navigation.navigate('Timer');
+  }
+  return (
+    <LinearGradient
+      colors={['#E7F3FE', '#9ABEE0']}
+      style={styles.container}
+    >
+      <Image style={styles.banner} source={bannerImg} />
+
+      <Text style={styles.title}>Staying focused at work isn't easy!</Text>
+
+      <TouchableOpacity style={styles.button} onPress={navigateToTimer}>
+        <MaterialIcons name="chevron-right" size={32} color="#FFF" />
+      </TouchableOpacity>
+    </LinearGradient>
+  );
 }
 
 export default Welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  banner: {
+    alignSelf: 'flex-end'
+  },
+
+  title: {
+    color: '#1C354F',
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    maxWidth: 280,
+    marginVertical: 80,
+  },
+
+  button: {
+    width: 72,
+    height: 72,
+    backgroundColor: '#2E5B9A',
+    borderRadius: 36,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+
+})
