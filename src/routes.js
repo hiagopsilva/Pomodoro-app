0be5c30e3b0b41cac07aa9a448a303f0f1ee9e01
hiagopsilva <hiagohps360@gmail.com>
2020-05-20 18:35:30 -0300

    add routes page

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+const Routes = () => (
+  <AppStack.Navigator>
+    <AppStack.Screen  />
+  </AppStack.Navigator>
+)
