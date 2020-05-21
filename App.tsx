a8c6faaa25fb3c43cff24c1f8008b16d5dbcd8a3
hiagopsilva <hiagohps360@gmail.com>
2020-05-20 21:28:52 -0300

    update statusBar style

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import { StatusBar } from 'react-native';
 import Routes from './src/routes';
 
 export default function App() {
   return (
+    <>
+    <StatusBar barStyle="default" backgroundColor="#1C354F" />
     <Routes />
+    </>
   );
 }
